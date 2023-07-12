778bc78d469c15ed3d84726143a884e3de82ec8e
Weidson Cordeiro <weidson.ac@gmail.com>
2023-07-12 15:01:27 -0700

    Melhorias diversas

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,25 +1,41 @@
 class MobileNavbar {
-    constructor(btnMenuSidBar, sidBarMenu) {
+    constructor(btnMenuSidBar, sidBarMenu,btnMenuMobileHome, btnMenuMobileComprar) {
         this.btnMenuSidBar = document.querySelector(btnMenuSidBar)
         this.sidBarMenu = document.querySelector(sidBarMenu)
-        //this.navLinks = document.querySelectorAll(navLinks)
+        this.btnMenuMobileHome = document.querySelector(btnMenuMobileHome)
+        this.btnMenuMobileComprar = document.querySelector(btnMenuMobileComprar)
         this.activeClass = "active"
         this.handleClick = this.handleClick.bind(this)
+        this.handleClickHome = this.handleClickHome.bind(this)
+        this.handleClickComprar = this.handleClickComprar.bind(this)
     }
 
     handleClick() {
         this.sidBarMenu.classList.toggle(this.activeClass)
     }
 
+    handleClickHome(){
+        if(this.sidBarMenu.classList.contains(this.activeClass)){
+            this.sidBarMenu.classList.remove(this.activeClass);
+        }
+    }
+
+    handleClickComprar(){
+        if(this.sidBarMenu.classList.contains(this.activeClass)){
+            this.sidBarMenu.classList.remove(this.activeClass);
+        }
+    }
+
     setListeners() {
         this.btnMenuSidBar.addEventListener("click", this.handleClick)
+        this.btnMenuMobileHome.addEventListener("click", this.handleClickHome)
+        this.btnMenuMobileComprar.addEventListener("click", this.handleClickComprar)
     }
 
     init() {
-        if (this.btnMenuSidBar) {
-            this.setListeners()
-        }
-
+        if (this.btnMenuSidBar) this.setListeners()
+        if (this.btnMenuMobileHome) this.setListeners()
+        if (this.btnMenuMobileComprar) this.setListeners()
     }
 
 }
