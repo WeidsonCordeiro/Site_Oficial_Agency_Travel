d5ea1699720b9f886693358d40356a5d78e866c2
Weidson Cordeiro <weidson.ac@gmail.com>
2023-07-12 15:17:20 -0700

    Ajustando chamada a Classe MobileNavbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-    const mobileNavBar = new MobileNavbar(".btn__mobile", ".menu__mobile",".menu__mobile .menu__mobile__list .menu__mobile__home", ".menu__mobile .menu__mobile__list .menu__mobile__comprar");
+    const mobileNavBar = new MobileNavbar(".btn__mobile", ".menu__mobile",".menu__mobile__home", ".menu__mobile__comprar");
     const carrousel = new Carrousel(".container-carrousel", ".box-destin");
     
     mobileNavBar.init();
